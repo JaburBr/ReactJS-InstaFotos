9c05d1b874020fbe8c1a43f4a9bb2de8f8342aec
JaburMacbookPro <jaburmail@gmail.com>
2018-05-20 19:31:15 -0300

    Criacao do repositorio

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './css/timeline.css';
+import './css/reset.css';
+import Header from './componentes/Header';
+import Timeline from "./componentes/Timeline";
+
+
+class App extends Component {
+  render() {
+    return (
+
+      <div id="root">
+        <div className="main">
+
+          <Header />
+          <Timeline />
+          
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/componentes/Timeline.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Foto from './Foto';
+
+export default class Timeline extends Component {
+
+    url = 'http://localhost:8080/api/public/fotos/alots';
+
+    constructor(){
+        super();
+        this.state = {fotos: []}
+    }
+    
+    componentDidMount(){
+
+        fetch(this.url)
+            .then(response => response.json())
+            .then(fotos => {
+                console.log(fotos);
+                this.setState({fotos:fotos});
+            })
+            .catch(err => console.log(err));
+
+    }
+
+    render() {
+        return (
+            <div className="fotos container">
+                <Foto />
+                <Foto />
+            </div>
+        );
+    }
+
+}
